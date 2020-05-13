0c3c68a11b1d0f9660ce3068dd5963b04ca75402
Slav <krikshchiukas.v@gmail.com>
2020-05-13 18:33:21 +0100

    Final version. Add note, edit, delete, search, hide.

--- NoteManager/script.js
+++ NoteManager/script.js
@@ -41,6 +41,94 @@ document.getElementById('add-btn').addEventListener('click', function(e) {
 
 
 
+//***********EDIT AND DELETE ITEMS
+
+ul.addEventListener('click', function(e) {
+
+    //e.target = this but in this case 'this' will return ul and e.target return children
+    //console.log(e.target.classList[1]);
+    
+        if(e.target.classList[1] === "fa-pencil-square-o") {
+    
+        //console.log('Hello');
+        // to hide paragraph
+            var parentPar = e.target.parentNode;
+            parentPar.style.display = 'none';
+    
+            var note = parentPar.previousElementSibling;
+            var input = parentPar.nextElementSibling;
+           // console.log(note, input);
+    
+           input.style.display = "block";
+           input.value = note.textContent;
+    
+           input.addEventListener('keypress', function(e) {
+           // console.log(e);
+    
+            if(e.keyCode === 13) {
+                if(input.value !== '') {
+                note.textContent = input.value;
+                parentPar.style.display = 'block';
+                input.style.display = 'none';
+                    } else {
+                        var li = input.parentNode;
+                        li.parentNode.removeChild(li);
+                  }           
+              } 
+          });
+        } else if(e.target.classList[1] === 'fa-times') {
+            var list = e.target.parentNode.parentNode;
+            list.parentNode.removeChild(list);
+        }
+    });
+    
+    
+    //***********HIDE ITEMES
+    
+    var hideItem = document.getElementById('hide');
+    
+    hideItem.addEventListener('click', function() {
+    
+        var label = document.querySelector('label');
+       
+        if(hideItem.checked) {
+            label.textContent = 'Unhide notes';
+    
+            ul.style.display = 'none';        
+        } else {
+            label.textContent = 'Hide notes';
+            ul.style.display = 'block';
+        }
+    })
+    
+    
+    //**************SEARCH FILTER
+    
+    var searchinput = document.querySelector('#search-note input');
+    
+    //keyup - event when key is released
+    
+    searchinput.addEventListener('keyup', function(e) {
+        //console.log('Key is released');
+    
+        //transfore input value in upper casr to make search case not sensetive
+        var searchChar = e.target.value.toUpperCase();
+       // console.log(searchChar);
+    
+       var notes = ul.getElementsByTagName('li');
+    
+       Array.from(notes).forEach(function(note) {
+            var parText = note.firstElementChild.textContent;
+    
+            if(parText.toUpperCase().indexOf(searchChar) !== -1) {
+                note.style.display = 'block';
+            } else {
+                note.style.display = 'none';
+            }        
+        });
+    
+    });
+    
 
 
 
